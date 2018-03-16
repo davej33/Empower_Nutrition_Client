02df5071a15e0eab84098e7d15d370decd16149a
Dave <davejusten03@yahoo.com>
2018-03-16 15:32:38 -0400

    added new firebase sign in functionality

--- app/src/main/java/com/example/davidjusten/empower_nutrition_client/FoodDetailActivity.java
+++ app/src/main/java/com/example/davidjusten/empower_nutrition_client/FoodDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.davidjusten.empower_nutrition_client;
 
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,13 +25,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class FoodDetailActivity extends AppCompatActivity {
+
 
     private String item_key = null;
     private DatabaseReference mDb;
     private TextView mDetailName, mDetailPrice, mDetailDesc;
     private ImageView mDetailImage;
-    private Button mOrderButton;
     private FirebaseAuth mAuth;
     private FirebaseUser current_user;
     private DatabaseReference user_data, mRef;
@@ -80,20 +89,23 @@ public class FoodDetailActivity extends AppCompatActivity {
 
             }
         });
+
     }
+
 
     public void purchaseItemClicked(View view) {
         final DatabaseReference newOrder = mRef.push();
         user_data.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.i("tag","datasnapshot: " + dataSnapshot.child("Name").getValue());
+                Log.i("tag", "datasnapshot: " + dataSnapshot.child("Name").getValue());
                 newOrder.child("itemName").setValue(food_name);
+                newOrder.child("time").setValue(timeOrdered());
                 newOrder.child("userName").setValue(current_user.getEmail()).addOnCompleteListener(
                         new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
-                                startActivity(new Intent(FoodDetailActivity.this, MenuActivity.class));
+                                startActivity(new Intent(FoodDetailActivity.this, ItemCategoryActivity.class));
                             }
                         }
                 );
@@ -104,5 +116,12 @@ public class FoodDetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private Object timeOrdered() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd @ hh:mm", Locale.US);
+        return df.format(cal.getTime());
+
     }
 }
--- app/src/main/java/com/example/davidjusten/empower_nutrition_client/ItemCategoryActivity.java
+++ app/src/main/java/com/example/davidjusten/empower_nutrition_client/ItemCategoryActivity.java
@@ -1,9 +1,17 @@
 package com.example.davidjusten.empower_nutrition_client;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class ItemCategoryActivity extends AppCompatActivity {
 
@@ -13,12 +21,55 @@ public class ItemCategoryActivity extends AppCompatActivity {
     private static final String JUICE = "Juice";
     private static final String TYPE = "type";
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private static final int RC_SIGN_IN = 1;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_category);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.i("tag", "is this running?????????????????????? ");
+                if (firebaseAuth.getCurrentUser() != null) {
+                    // signed in
+                    Toast.makeText(ItemCategoryActivity.this, "You're signed in!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // not signed in
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()
+                                    ))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAuth.addAuthStateListener(mAuthStateListener);
+        Log.i("tag", "is this running?????????????????????? ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAuth.removeAuthStateListener(mAuthStateListener);
+    }
     public void smoothieClicked(View view) {
         Intent smoothieIntent = new Intent(ItemCategoryActivity.this, MenuActivity.class);
         smoothieIntent.putExtra(TYPE, SMOOTHIE);
